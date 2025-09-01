3b73f72574a5bf1b9967eec06ac65d4e1a4c196f
irw9n <irwanahmed001@gmail.com>
2025-09-01 19:54:41 +0800

    iP Level-1

--- src/main/java/Ian.java
+++ src/main/java/Ian.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+
 public class Ian {
+    public static void echoUserInput(String input) {
+        System.out.println("____________________________________________________________");
+        System.out.println(input);
+        System.out.println("____________________________________________________________");
+    }
+
     public static void main(String[] args) {
 
+        System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Ian");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+
+        do {
+            userInput = scanner.nextLine();
+
+            if (userInput.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            echoUserInput(userInput);
+
+        } while (true);
+
+        scanner.close();
+
     }
 }
